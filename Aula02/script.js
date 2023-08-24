291430e6b6aeae765c908d50a9e0f10a4ac2681e
Ítalo Nunes <37880520+italo2403@users.noreply.github.com>
2023-08-24 10:47:36 -0300

    Aula 02
    Assuntos da Aula
    Listas
    Metas
    Divs
    Forms

---
+++ Aula02/script.js
@@ -0,0 +1,9 @@
+let v1=  Number(prompt("Insira um valor de pagamento"));
+let v2 = parseFloat(prompt("Insira um valor de pagamento"));
+let total = v1+v2;
+console.log(total);
+document.write(total);
+alert(total)
+
+
+
